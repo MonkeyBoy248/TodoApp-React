305b1aefe24e93d525bc89f096536e0569b0f58b
Kirill Temnyi <kirik201100@mail.ru>
2023-04-04 17:53:02 +0300

    feat: create AddTodoForm component

--- src/features/Todo/components/AddTodoForm/AddTodoForm.tsx
+++ src/features/Todo/components/AddTodoForm/AddTodoForm.tsx
@@ -1,5 +1,56 @@
+import { TextButton } from '../../../../components/TextButton/TextButton';
+import styles from './AddTodoForm.module.scss';
+
 export const AddTodoForm = () => {
   return (
-    <div>AddTodoForm</div>
-  )
-}
+    <form
+      className={styles.addTodoForm}
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log('Submitted');
+      }}
+    >
+      <fieldset className={styles.addTodoForm__fields}>
+        <legend className={styles.addTodoForm__legend}>Add a new todo</legend>
+        <ul className={styles.addTodoForm__fieldsList}>
+          <li className={styles.addTodoForm__fieldWrapper}>
+            <label
+              className={styles.addTodoForm__label}
+              htmlFor='addTodoFrom__titleInput'
+            >
+              Todo title
+            </label>
+            <input
+              className={styles.addTodoForm__input}
+              type='text'
+              name='title'
+              id='addTodoForm__titleInput'
+              placeholder='Enter a todo title'
+              required={true}
+              minLength={3}
+              maxLength={30}
+            />
+          </li>
+          <li className={styles.addTodoForm__fieldWrapper}>
+            <label
+              className={styles.addTodoForm__label}
+              htmlFor='addTodoFrom__textInput'
+            >
+              Todo text
+            </label>
+            <input
+              className={styles.addTodoForm__input}
+              type='text'
+              name='title'
+              id='addTodoForm__textInput'
+              placeholder='Enter a todo text'
+              minLength={3}
+              maxLength={50}
+            />
+          </li>
+        </ul>
+        <TextButton text={'Add todo'} type={'submit'} />
+      </fieldset>
+    </form>
+  );
+};
