26c59440b74eb8ed96d1539adffef7bc8fe4d41e
Kirill Temnyi <kirik201100@mail.ru>
2023-04-04 16:05:58 +0300

    feat: create TodoList component

--- src/features/Todo/components/TodoList/TodoList.tsx
+++ src/features/Todo/components/TodoList/TodoList.tsx
@@ -1,5 +1,17 @@
-export const TodoList = () => {
-  return (
-    <div>TodoList</div>
-  )
+import { Todo } from '../../../../types';
+import { TodoItem } from '../TodoItem/TodoItem';
+import styles from './TodoList.module.scss';
+
+interface TodoListProps {
+  todos: Todo[];
 }
+
+export const TodoList = ({ todos }: TodoListProps) => {
+  return (
+    <ul className={styles.todoList}>
+      {todos.map((todo) => {
+        return <TodoItem todo={todo} key={todo.id} />;
+      })}
+    </ul>
+  );
+};
