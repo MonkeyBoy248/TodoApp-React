999ea016ef7d48b0598ee5a0ed3d3303cdcf9a62
Kirill Temnyi <kirik201100@mail.ru>
2023-04-04 16:06:20 +0300

    feat: create TodoContainer component

--- src/features/Todo/components/TodoContainer/TodoContainer.tsx
+++ src/features/Todo/components/TodoContainer/TodoContainer.tsx
@@ -1,17 +1,40 @@
 import styles from './TodoContainer.module.scss';
 import { AddTodoForm } from '../AddTodoForm/AddTodoForm';
 import { TodoList } from '../TodoList/TodoList';
+import { TodoFooter } from '../TodoFooter/TodoFooter';
 
 export const TodoContainer = () => {
+  const todos = [
+    {
+      id: 'kglt',
+      title: 'Contest',
+      done: false,
+      text: 'Finish context',
+    },
+    {
+      id: 'klko',
+      title: 'State manger',
+      done: false,
+      text: 'Create state manager',
+    },
+    {
+      id: 'jkhu',
+      title: 'Todo app',
+      done: false,
+      text: 'Finish the project',
+    },
+  ];
+  
   return (
     <main className={styles.todo__container}>
       <div className={styles.todo__mainContent}>
         <header className={styles.todo__header}>
           <h2 className={styles.todo__title}>Todos</h2>
-            <AddTodoForm></AddTodoForm>
+          <AddTodoForm></AddTodoForm>
         </header>
-        <TodoList />
+        <TodoList todos={todos} />
+        <TodoFooter />
       </div>
     </main>
-  )
-}
+  );
+};
